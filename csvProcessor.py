100594386ff7d6cba09d52681f4af12187e43bf9
Sean Feeney <sfeeney@guaranteedrate.com>
2023-07-11 17:06:23 -0700

    in progress, don't use yet

--- csvProcessor.py
+++ csvProcessor.py
@@ -3,20 +3,36 @@ import datetime
 from rates import *
 
 def processInput(input):
-    dataFrame = pd.read_csv(input, parse_dates=[['Usage date', 'Interval']], date_format='%m/%d/%y %I:%M %p')
-    #print(dataFrame['Usage date_Interval'])
-#    for row in dataFrame.itertuples():
-#        month = 
-#    print(dataFrame['Usage date'])
+    #dataFrame = pd.read_csv(input, parse_dates=[['Usage date','Interval']], date_format='%m/%d/%y %I:%M %p')
+    dataFrame = pd.read_csv(input)
 
-    dataFrame['Month'] = pd.DatetimeIndex(dataFrame['Usage date_Interval']).strftime('%B').str.lower()
-    dataFrame['Hour'] = pd.DatetimeIndex(dataFrame['Usage date_Interval']).hour
+    dataFrame['Datetime'] = pd.to_datetime(dataFrame['Usage date'] + ' ' + dataFrame['Interval'], format='mixed')
+    dataFrame['Month'] = pd.DatetimeIndex(dataFrame['Datetime']).strftime('%B').str.lower()
+    dataFrame['Hour'] = pd.DatetimeIndex(dataFrame['Datetime']).hour
+    dataFrame['Date'] = pd.DatetimeIndex(dataFrame['Datetime']).strftime('%x')
+    
+    dataFrame.set_index('Datetime', inplace=True)
 
+    print(dataFrame)
+
+    # Find interval during on-peak hours when your home used the most electricity
+    
+    filter = dataFrame['On-peak kWh'] > 0
+    maxes = dataFrame.where(filter).groupby('Date', as_index=False).max('On-peak kWh')
+    #recombine to index
+    maxes['Hour_int'] = maxes['Hour'].astype(int)
+    maxes['Datetime'] = pd.to_datetime(maxes['Date'] + ' ' + maxes['Hour_int'].astype(str), format='mixed')
+    maxes.set_index('Datetime', inplace=True)
+
+    # Add it back to the dataset
+    dataFrame['IsDailyMax'] = dataFrame.apply(findDailyMax, args=(maxes,), axis=1)
+
+    print(dataFrame)
+
+    # Fill in costs per plan
     dataFrame['e27Cost'] = dataFrame.apply(calcE27Cost, axis=1)
 
     #print(demandPlans.month['june']['hour'][0])
-
-    #print (dataFrame)
 
     return dataFrame
 
@@ -26,5 +42,13 @@ def calcE27Cost(row):
         return row['Off-peak kWh'] * demandPlans.month[row['Month']]['hour'][row['Hour']]
     else:
         #On Peak
-        #TODO
-        return 0
+        peakCalc = row['On-peak kWh'] * demandPlans.month[row['Month']]['hour'][row['Hour']]
+        # TODO: figure out the day's max On-peak kWh, run thru cost calcs on it
+        return peakCalc
+    
+def findDailyMax(row, maxes):
+    if row.name in maxes.index:
+        return True
+    else:
+        return False
+
